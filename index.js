f599fe8d1ed97a8f9c2830179d1d770580e03ba8
Katherine Struthers <ktstruthers@gmail.com>
2023-05-21 19:42:54 -0500

    add api to fetch ISS fly over times

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+
+// fetchISSFlyOverTimes({ latitude: 49.8997541, longitude: -97.1374937 }, (error, data) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+
+//   console.log(data);
+// });
 
 // fetchCoordsByIP('42', (error, data) => {
 //   if (error) {
