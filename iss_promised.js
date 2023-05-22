1e0107ee080bdeea7fb0b0186e67abf6b8599f67
Katherine Struthers <ktstruthers@gmail.com>
2023-05-21 22:02:15 -0500

    same premise only rewritten using promises instead of callbacks

---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+  
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const latitude = JSON.parse(body).latitude;
+  const longitude = JSON.parse(body).longitude;
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((body) => {
+      const { response } = JSON.parse(body);
+      return response;
+    });
+};
+
+
+module.exports = { nextISSTimesForMyLocation };
+
